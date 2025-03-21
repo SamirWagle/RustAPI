e8708c44f416cc6c06ba9d0833452ec866c8f0ec
[Samir Wagle] <[waglesameer5@gmail.com]>
2025-03-21 20:35:02 +0545

    first

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+/// Adds one to the given number.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = samir_practice::add_one(arg);
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    println!("This is made by Mr Sameer Wagle");
+    x+1
+}
